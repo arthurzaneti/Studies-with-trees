1b2b0fdfd0444e3caf60cfbce8ff3093e83e4058
Arthur Zaneti <afzaneti1@gmail.com>
2023-05-09 10:12:49 -0300

    still didn't get the nivel_no function, but got the rest

--- main.c
+++ main.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+#include "tree_func.c"
 
-typedef struct _no{
-    char data;
-    struct _no*dir;
-    struct _no*esq;
-}no;
 
-no cria_no(char data, no*dir, no*esq){
-    no *novo_no = malloc(sizeof(no));
-    if (novo_no==NULL) {
-        print("Erro ao criar no");
-        return;
-    }
-    novo_no->data = data;
-    novo_no->dir = dir;
-    novo_no->esq = esq;
-    return *novo_no;
+int main(){
+    arv_t* arv = cria_arv('a', cria_arv('b', cria_arv('d',NULL,NULL), cria_arv('e',NULL,NULL)),cria_arv('c',NULL, NULL));
+    printf("Num_nos: %d\n", num_nos(arv));
+    printf("Altura: %d\n", altura(arv));
+    printf("Num_nos_folha: %d\n", num_nos_folha(arv));
+    printf("Num_nos_nao_folha: %d\n", num_nos_nao_folha(arv));
+    printf("Nivel_no: %d\n", nivel_no(arv, 'd'));
+    return 0;
 }
