40638ba98a1241fc5c1e7f2ecb3354c42e3083b2
Arthur Zaneti <afzaneti1@gmail.com>
2023-05-24 09:31:53 -0300

    No bugs anymore

--- ABB/main.c
+++ ABB/main.c
@@ -4,10 +4,17 @@
 #include <stdio.h>
 
 int main(){
-    arv_t*minha_arv = cria_arv(100, NULL, NULL);
+    arv_t*minha_arv = cria_arv(rand(), NULL, NULL);
     for (int i=0; i<1000; i++){
-        insere(minha_arv, i);
+        insere(minha_arv, rand());
     }
-    pos_ordem_esq(minha_arv);
+    
+    //pre_ordem_esq(minha_arv);
+    //em_ordem_esq(minha_arv);
+    //pos_ordem_esq(minha_arv);
+    //pre_ordem_dir(minha_arv);
+    //em_ordem_dir(minha_arv);
+    //pos_ordem_dir(minha_arv);
+    
     return 0;
 }
